d1fe2b45ba5633f78a860be49266cd13ea2efaf1
sefatanam <sefatanam@gmail.com>
2021-11-05 10:57:22 +0600

    Initial Commit

---
+++ src/app/screens/tabs/tabs-routing.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+import {TabsPage} from './tabs.component';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+          }
+        ]
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
---
+++ src/app/screens/tabs/tabs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss'],
+})
+export class TabsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
