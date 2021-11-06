eb9dce10fc8b880105be5fef230a5dd6f7201d21
sefatanam <sefatanam@gmail.com>
2021-11-06 13:13:03 +0600

    now splash can forward to another page

--- src/app/screens/splash/splash.page.ts
+++ src/app/screens/splash/splash.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ViewDidEnter } from "@ionic/angular";
+import { of } from "rxjs";
+import { delay } from "rxjs/operators";
 
 @Component({
   selector: 'app-splash',
   templateUrl: './splash.page.html',
   styleUrls: ['./splash.page.scss'],
 })
-export class SplashPage implements OnInit {
+export class SplashPage implements ViewDidEnter {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  ionViewDidEnter(): void {
+
+    of('ionViewDidEnter').pipe(
+      delay(2500),
+    ).subscribe(() => this.router.navigate(['/app/tabs/home']));
+
   }
+
 
 }
